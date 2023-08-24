6379070a0d3c59cbad7418828286185fb832c2fb
Miguel_Avalos <miguel.avalos@utec.edu.pe>
2023-08-24 00:44:38 -0500

    commit

--- app.py
+++ app.py
@@ -6,26 +6,10 @@ lista_alumnos = [{"nombre":"Miguel","mail":"abc@gmail.com","carrera":"Data Scien
 
 
 
-@app.route('/',methods = ['GET'])
-def Registro():
-    return render_template("index.html")
-@app.route('/registro',methods = ['POST'])
-def Mostrar_Registro():
-    name = request.form.get("name")
-    mail = request.form.get("mail")
-    carrera = request.form.get("carrera")
-    id = request.form.get("id")
-    if not name or not carrera or not mail or not id:
-        return render_template("Error.html")
-    lista_alumnos.append(f"Registered: {name};  {mail};  {carrera}; {id}")
-    return redirect("/registro_alumnos")
-@app.route('/registros_alumnos')
-def registrantes():
-    return render_template("registrados.html", lista_alumnos = lista_alumnos)
-
-
-
+@app.route('/')
+def Mostrar():
+    return "hello world"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
